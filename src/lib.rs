ca144c6c7b466a851009fc9bb8e2388628f27994
Oliver Giersch <oliver.giersch@b-tu.de>
2022-06-01 17:31:39 +0200

    fleshed out impl & API

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ mod stream;
 
 use std::{borrow::Cow, future::Future};
 
+use stream::TaskError;
 use tokio::{
     sync::{mpsc, oneshot},
     task,
@@ -10,85 +11,225 @@ use tokio_stream::StreamExt;
 
 use crate::stream::JoinHandleStream;
 
-type TaskName = Cow<'static, String>;
+type TaskName = Cow<'static, str>;
 
 // TODO: handle sender
 // TODO: error receiver (mut)
 pub struct TaskGroup<E> {
     tx: mpsc::UnboundedSender<Event<E>>,
-    err_rx: (),
+    done_rx: Option<oneshot::Receiver<GroupResult<E>>>,
 }
 
-impl<E> TaskGroup<E> {
+impl<E: Send + 'static> TaskGroup<E> {
+    /// # Panics
+    ///
+    /// Panics, if this is called outside of a tokio runtime.
     pub fn new() -> Self {
-        todo!()
+        Self::with_capacity(0, false)
     }
 
-    pub fn close(self) -> Result<(), ()> {
-        // send close message
-        todo!()
+    pub fn local() -> Self {
+        Self::with_capacity(0, true)
+    }
+
+    pub fn with_capacity(capacity: usize, local: bool) -> Self {
+        let (tx, rx) = mpsc::unbounded_channel();
+        let (done_tx, done_rx) = oneshot::channel();
+
+        if local {
+            let _ = task::spawn_local(group_manager(capacity, done_tx, rx));
+        } else {
+            let _ = task::spawn(group_manager(capacity, done_tx, rx));
+        }
+
+        Self {
+            tx,
+            done_rx: Some(done_rx),
+        }
+    }
+
+    /// Closes the [`TaskGroup`]
+    pub fn close(mut self) -> JoinGroupHandle<E> {
+        let done_rx = self
+            .done_rx
+            .take()
+            .expect("task group error has already been observed");
+
+        // send the close message to group manager
+        let res = self.tx.send(Event::Closed);
+        assert!(res.is_ok(), "group manager must be available for close msg");
+
+        JoinGroupHandle { done_rx }
+    }
+
+    pub fn spawn<F>(&self, name: impl Into<TaskName>, future: F)
+    where
+        F: Future<Output = Result<(), E>> + Send + 'static,
+    {
+        // spawn
+        let handle = task::spawn(future);
+        let event = Event::Handle(name.into(), handle);
+
+        let res = self.tx.send(event);
+        assert!(res.is_ok(), "spawning must always succeed");
+    }
+
+    /// ...
+    ///
+    /// # Panics
+    ///
+    /// ...
+    pub async fn errored(&mut self) -> (TaskName, TaskError<E>) {
+        let done_rx = self
+            .done_rx
+            .take()
+            .expect("task group error has already been observed");
+
+        match done_rx.await {
+            Ok(Ok(_)) | Err(_) => unreachable!("TODO: EXPLAIN"),
+            Ok(Err((name, err))) => (name, err),
+        }
     }
 
     // TODO: can fail, even if not close
-    pub fn spawn<T>(name: impl Into<TaskName>, future: T)
-    where
-        T: Future + Send + 'static,
-        T::Output: Send + 'static,
-    {
-        // spawn
-        task::spawn(future);
+}
+
+pub struct JoinGroupHandle<E> {
+    done_rx: oneshot::Receiver<GroupResult<E>>,
+}
+
+impl<E> JoinGroupHandle<E> {
+    pub async fn join(self) -> Result<(), (TaskName, TaskError<E>)> {
+        self.done_rx.await.expect("TODO")
     }
 }
 
-pub struct SpawnErr;
+#[derive(Debug)]
+pub struct SpawnError;
 
 enum Event<E> {
     Handle(TaskName, task::JoinHandle<Result<(), E>>),
     Closed,
 }
 
+type GroupResult<E> = Result<(), (TaskName, TaskError<E>)>;
+
 async fn group_manager<E>(
     capacity: usize,
-    tx: oneshot::Sender<(TaskName, Result<Result<(), E>, task::JoinError>)>,
+    done_tx: oneshot::Sender<GroupResult<E>>,
     mut rx: mpsc::UnboundedReceiver<Event<E>>,
 ) {
     let mut stream = JoinHandleStream::<E>::with_capacity(capacity);
-    let mut closed = false;
+    let mut is_closed = false;
 
-    let (task_name, err) = loop {
+    let mut group_res: GroupResult<E> = Ok(());
+    loop {
         let next = stream.next();
         tokio::pin!(next);
 
         tokio::select! {
             biased;
+            // a task has completed or there are no more tasks
             res = &mut next => match res {
-                Some((name, res)) => if let Err(e) = res {
-                    stream.abort_all().await;
+                // the completed task had an error
+                Some(res) => if let Err(e) = res {
+                    // close the channel to prevent spawning further tasks, but
+                    // keep handling any that have already been queued, i.e.,
+                    // don't break yet
                     rx.close();
-                    break (name, e);
+                    group_res = Err(e);
                 },
-                // the task group is closed and all tasks have finished
-                None => {
-                    todo!("close")
-                },
+                // the task group has been closed and all tasks have finished
+                None => break,
             },
             msg = rx.recv() => match msg {
                 Some(Event::Handle(name, handle)) => {
-                    assert!(!closed);
-                    stream.insert(name, handle);
+                    assert!(is_closed == false);
+                    stream.insert(name, handle)
                 },
                 Some(Event::Closed) => {
-                    closed = true;
+                    is_closed = true;
+                    rx.close();
                     stream.close();
                 },
                 None => {
-                    // if the receiver is just dropped, cancel all tasks
+                    // if the group handle is just dropped (without sending a
+                    // close message), cancel all tasks
                     stream.abort_all().await;
-                    return;
+                    break;
                 },
             }
         }
-    };
+    }
+
+    let _ = done_tx.send(group_res);
 
     // todo: cancel all handles still in rx
+}
+
+fn convert_task_result() {}
+
+#[cfg(test)]
+fn block_on<F: Future<Output = ()> + Send>(f: F) {
+    tokio::runtime::Builder::new_current_thread()
+        .enable_time()
+        .build()
+        .unwrap()
+        .block_on(f)
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{
+        sync::{
+            atomic::{AtomicBool, Ordering},
+            Arc,
+        },
+        time::Duration,
+    };
+
+    use super::TaskGroup;
+
+    #[test]
+    fn close() {
+        crate::block_on(async {
+            let group = TaskGroup::<()>::new();
+
+            group.spawn("a", async { Ok(()) });
+            group.spawn("b", async { Ok(()) });
+            group.spawn("c", async { Ok(()) });
+
+            let handle = group.close();
+            let res = handle.join().await;
+            assert!(res.is_ok())
+        })
+    }
+
+    #[test]
+    fn cancel() {
+        struct OnCancel(Arc<AtomicBool>);
+
+        impl Drop for OnCancel {
+            fn drop(&mut self) {
+                self.0.store(true, Ordering::Relaxed);
+            }
+        }
+
+        crate::block_on(async {
+            let cancelled = Arc::default();
+
+            let group = TaskGroup::<()>::new();
+
+            let flag = Arc::clone(&cancelled);
+            group.spawn("a", async move {
+                let _guard = OnCancel(flag);
+                std::future::pending().await
+            });
+
+            drop(group);
+            tokio::task::yield_now().await;
+
+            assert!(cancelled.load(Ordering::Relaxed));
+        })
+    }
 }
--- src/stream.rs
+++ src/stream.rs
@@ -1,4 +1,5 @@
 use std::{
+    cmp, fmt,
     future::Future,
     pin::Pin,
     task::{Context, Poll},
@@ -9,9 +10,35 @@ use tokio_stream::{Stream, StreamExt};
 
 use crate::TaskName;
 
-type TaskJoinResult<E> = Result<Result<(), E>, task::JoinError>;
+type TaskJoinResult<E> = Result<(), (TaskName, TaskError<E>)>;
 
-pub(crate) struct JoinHandleStream<E> {
+#[derive(Debug)]
+pub enum TaskError<E> {
+    Panic,
+    Error(E),
+}
+
+impl<E: cmp::PartialEq> cmp::PartialEq for TaskError<E> {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (Self::Error(e0), Self::Error(e1)) => e0.eq(e1),
+            (Self::Panic, Self::Panic) => true,
+            _ => false,
+        }
+    }
+}
+
+impl<E: cmp::Eq> cmp::Eq for TaskError<E> {}
+
+impl<E: fmt::Display> fmt::Display for TaskError<E> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        todo!()
+    }
+}
+
+/// A stream of task join handles which produces an item whenever a task handle
+/// completes.
+pub(crate) struct JoinHandleStream<E = ()> {
     handles: Vec<(TaskName, task::JoinHandle<Result<(), E>>)>,
     closed: bool,
 }
@@ -29,7 +56,7 @@ impl<E> JoinHandleStream<E> {
     }
 
     pub(crate) fn insert(&mut self, name: TaskName, handle: task::JoinHandle<Result<(), E>>) {
-        debug_assert!(!self.closed);
+        debug_assert!(!self.closed, "can not insert into closed stream");
         self.handles.push((name, handle));
     }
 
@@ -42,25 +69,31 @@ impl<E> JoinHandleStream<E> {
         while let Some(_) = self.next().await {}
     }
 
-    fn poll_next_join(
-        &mut self,
-        cx: &mut Context<'_>,
-    ) -> Poll<Option<(TaskName, TaskJoinResult<E>)>> {
+    fn poll_next_join(&mut self, cx: &mut Context<'_>) -> Poll<Option<TaskJoinResult<E>>> {
         for i in 0..self.handles.len() {
             let (_, handle) = &mut self.handles[i];
             if let Poll::Ready(res) = Pin::new(handle).poll(cx) {
                 let (name, _) = self.handles.swap_remove(i);
                 self.shrink_if_oversized();
 
-                return Poll::Ready(Some((name, res)));
+                let res = match res {
+                    Ok(Ok(_)) => Ok(()),
+                    Ok(Err(e)) => Err((name, TaskError::Error(e))),
+                    Err(join) => match join.try_into_panic() {
+                        Ok(_) => Err((name, TaskError::Panic)),
+                        Err(_) => Ok(()),
+                    },
+                };
+
+                return Poll::Ready(Some(res));
             }
         }
 
-        if self.closed {
-            Poll::Ready(None)
-        } else {
-            Poll::Pending
+        if self.closed && self.handles.is_empty() {
+            return Poll::Ready(None);
         }
+
+        Poll::Pending
     }
 
     fn shrink_if_oversized(&mut self) {
@@ -71,9 +104,101 @@ impl<E> JoinHandleStream<E> {
 }
 
 impl<E> Stream for JoinHandleStream<E> {
-    type Item = (TaskName, TaskJoinResult<E>);
+    type Item = TaskJoinResult<E>;
 
     fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
         self.poll_next_join(cx)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use tokio::task;
+    use tokio_stream::StreamExt;
+
+    use crate::stream::TaskError;
+
+    use super::JoinHandleStream;
+
+    #[test]
+    fn close() {
+        crate::block_on(async {
+            let mut stream = JoinHandleStream::<()>::with_capacity(2);
+
+            stream.insert("a".into(), task::spawn(async { Ok(()) }));
+            stream.insert("b".into(), task::spawn(async { Ok(()) }));
+
+            stream.close();
+
+            let next = stream.next().await;
+            assert_eq!(next, Some(Ok(())));
+            let next = stream.next().await;
+            assert_eq!(next, Some(Ok(())));
+            let next = stream.next().await;
+            assert!(next.is_none());
+        });
+    }
+
+    #[test]
+    fn abort() {
+        crate::block_on(async {
+            let mut stream = JoinHandleStream::<()>::with_capacity(2);
+
+            stream.insert("a".into(), task::spawn(async { Ok(()) }));
+            stream.insert("b".into(), task::spawn(std::future::pending()));
+
+            stream.abort_all().await;
+
+            let next = stream.next().await;
+            assert!(next.is_none());
+        })
+    }
+
+    #[test]
+    fn error() {
+        crate::block_on(async {
+            let mut stream = JoinHandleStream::<i32>::with_capacity(2);
+
+            stream.insert("a".into(), task::spawn(async { Ok(()) }));
+            stream.insert("b".into(), task::spawn(async { Err(-1) }));
+
+            let r0 = stream.next().await.unwrap();
+            let r1 = stream.next().await.unwrap();
+
+            let (name, err) = match (r0, r1) {
+                (Ok(_), Err(e)) | (Err(e), Ok(_)) => e,
+                _ => panic!("expected exactly one task failure"),
+            };
+
+            assert_eq!(name, "b");
+            assert_eq!(err, TaskError::Error(-1));
+        });
+    }
+
+    #[test]
+    fn panic() {
+        let hook = std::panic::take_hook();
+        std::panic::set_hook(Box::new(|_| {}));
+
+        crate::block_on(async {
+            let mut stream = JoinHandleStream::<i32>::with_capacity(2);
+
+            stream.insert("a".into(), task::spawn(async { Ok(()) }));
+            stream.insert("b".into(), task::spawn(async { panic!("fatal") }));
+
+            let r0 = stream.next().await.unwrap();
+            let r1 = stream.next().await.unwrap();
+
+            let (name, err) = match (r0, r1) {
+                (Ok(_), Err(e)) | (Err(e), Ok(_)) => e,
+                _ => panic!("expected exactly one task failure"),
+            };
+
+            assert_eq!(name, "b");
+            assert_eq!(err, TaskError::Panic);
+        });
+
+        std::panic::set_hook(hook);
     }
 }
